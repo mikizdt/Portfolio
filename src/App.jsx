eba512118bfc0d695a557b3313f7269e07f108f4
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-10-09 00:01:57 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./components/navbar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar bg-light ">
+      <ul>
+        <li>Portfolio</li>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Contacts</li>
+      </ul>
+    </nav>
+  );
+}
